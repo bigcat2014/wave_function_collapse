07c62df732f24507bdb391e0fe219802e3bf0168
Logan Thomas <ltogan.2014@gmail.com>
2025-06-08 19:34:01 -0400

    Initial library implementation

--- include/wave_function_collapse/wave_function_collapse.hpp
+++ include/wave_function_collapse/wave_function_collapse.hpp
@@ -1,58 +1,244 @@
 #pragma once
 
+#include <limits.h>
+
 #include <boost/graph/adjacency_list.hpp>
-#include <boost/graph/adjacency_matrix.hpp>
-#include <boost/graph/graphviz.hpp>
-#include <boost/graph/named_function_params.hpp>
+#include <boost/graph/breadth_first_search.hpp>
 #include <boost/graph/properties.hpp>
-#include <boost/property_map/property_map.hpp>
+#include <iostream>
+#include <iterator>
+#include <memory>
+#include <random>
 
 namespace wfc {
 
 template <typename T>
-struct RelVertexData {
-  T data;
-};
-
-template <typename T>
-struct BoardVertexData : public RelVertexData<T> {
-  std::vector<RelVertexData&> potential_states;
-  void entropy() { return potential_states.size(); };
-};
-
-struct EdgeData {
-  unsigned dimension;
-};
-
-template <typename T>
-class wfc {
+class WaveFunctionCollapse {
  private:
+  struct RelationshipVertex;
+  struct BoardVertex;
+  struct EdgeData;
   using Relationships =
-      boost::adjacency_list<boost::vecS, boost::vecS, boost::directedS,
-                            RelVertexData<T>, EdgeData, boost::no_property,
+      boost::adjacency_list<boost::vecS, boost::vecS, boost::bidirectionalS,
+                            RelationshipVertex, EdgeData, boost::no_property,
                             boost::listS>;
+
   using Board =
-      boost::adjacency_matrix<boost::undirectedS, BoardVertexData<T>, EdgeData,
-                              boost::no_property, std::allocator<bool>>;
+      boost::adjacency_list<boost::vecS, boost::vecS, boost::bidirectionalS,
+                            BoardVertex, EdgeData, boost::no_property,
+                            boost::listS>;
 
   Relationships m_Relationships;
   Board m_Board;
+  bool initialized = false;
 
  public:
-  bool addRelationship(unsigned v1, unsigned v2, unsigned dimension) {
-    return false;
+  WaveFunctionCollapse(typename Board::vertices_size_type size)
+      : m_Board(size) {}
+
+  void init() {
+    auto bVpair = vertices(m_Board);
+    auto rVpair = vertices(m_Relationships);
+
+    // Initialize potential states
+    for (auto iter = bVpair.first; iter != bVpair.second; iter++) {
+      m_Board[*iter].potentialStates = std::vector(rVpair.first, rVpair.second);
+    }
+    initialized = true;
   }
 
-  bool addNeighbor(unsigned v1, unsigned v2) { return false; }
+  typename Relationships::vertex_descriptor addResource(
+      const std::weak_ptr<T> data) {
+    auto v = add_vertex({data}, m_Relationships);
+    initialized = false;
+    return v;
+  }
+
+  bool addRelationship(typename Relationships::vertex_descriptor v1,
+                       typename Relationships::vertex_descriptor v2,
+                       unsigned dimension) {
+    auto epair = add_edge(v1, v2, {dimension}, m_Relationships);
+    initialized = false;
+    return epair.second;
+  }
+
+  bool addNeighbor(typename Board::vertex_descriptor v1,
+                   typename Board::vertex_descriptor v2, unsigned dimension) {
+    auto epair = add_edge(v1, v2, {dimension}, m_Board);
+    initialized = false;
+    return epair.second;
+  }
+
+  std::shared_ptr<T> getData(typename Board::vertex_descriptor v) {
+    BoardVertex bV = m_Board[v];
+    if (bV.entropy() > 1) {
+      throw new std::domain_error("Tile has not been collapsed.");
+    }
+
+    return m_Relationships[bV.potentialStates[0]].data.lock();
+  }
+
+  void collapse(const typename Board::vertex_descriptor bVd,
+                const typename Relationships::vertex_descriptor rVd) {
+    if (!initialized) {
+      throw new std::domain_error("WFC must be initialized");
+    }
+
+    auto bIter = m_Board[bVd].potentialStates.begin();
+    auto eIter = m_Board[bVd].potentialStates.end();
+    if (std::find(bIter, eIter, rVd) == eIter) {
+      throw new std::domain_error("Cannot collapse vertex to specified state.");
+    }
+
+    m_Board[bVd].potentialStates = {rVd};
+    boost::breadth_first_search(m_Board, bVd,
+                                boost::visitor(Visitor(m_Relationships)));
+  }
+
+  void run() {
+    if (!initialized) {
+      throw new std::domain_error("WFC must be initialized");
+    }
+
+    auto bVpair = vertices(m_Board);
+
+    // Select random starting vertex
+    typename Board::vertex_descriptor min =
+        *selectRandom(bVpair.first, bVpair.second);
+
+    // Collapse a vertex on every iteration
+    for (unsigned i = 0; i < num_vertices(m_Board); i++) {
+      // Collapse the selected vertex with a random state
+      collapse(min, *selectRandom(m_Board[min].potentialStates.begin(),
+                                  m_Board[min].potentialStates.end()));
+
+      // Find the vertex with the lowest entropy, excluding collapsed vertices
+      min = UINT_MAX;
+      for (auto vIter = bVpair.first; vIter != bVpair.second; vIter++) {
+        if (m_Board[*vIter].entropy() <= 1) {
+          continue;
+        }
+        if (min == UINT_MAX ||
+            (m_Board[*vIter].entropy() < m_Board[min].entropy())) {
+          min = *vIter;
+        }
+      }
+
+      // If no uncollapsed vertices have been found, exit early
+      if (min == UINT_MAX) {
+        break;
+      }
+    }
+  }
+
+  void printBoard() {
+    std::cout << "\nBoard:" << std::endl;
+    auto vpair = vertices(m_Board);
+    for (auto iter = vpair.first; iter != vpair.second; iter++) {
+      std::cout << "vertex: " << *iter;
+      if (m_Board[*iter].potentialStates.size() == 1) {
+        std::cout << " data: " << m_Board[*iter].potentialStates[0];
+      }
+      std::cout << " entropy: " << m_Board[*iter].entropy() << std::endl;
+    }
+
+    // auto epair = edges(B);
+    // for (auto iter = epair.first; iter != epair.second; iter++) {
+    //   std::cout << "edge " << source(*iter, B) << " - " << target(*iter, B)
+    //             << " has dimension " << get(&EdgeData::dimension, B)[*iter]
+    //             << std::endl;
+    // }
+  }
 
  private:
-  inline T& getRelationshipData(unsigned vertex) {
-    get(&RelVertexData<T>::data, m_Relationships)[vertex];
+  struct RelationshipVertex {
+    const std::weak_ptr<T> data;
+  };
+
+  struct BoardVertex {
+    std::vector<
+        typename Relationships::vertex_descriptor> mutable potentialStates;
+    unsigned entropy() const { return potentialStates.size(); };
+  };
+
+  struct EdgeData {
+    unsigned dimension;
+  };
+
+  template <typename Iter, typename RandomGenerator>
+  Iter selectRandom(Iter start, Iter end, RandomGenerator& g) {
+    std::uniform_int_distribution<> dis(0, std::distance(start, end) - 1);
+    std::advance(start, dis(g));
+    return start;
   }
 
-  inline T& getBoardData(unsigned vertex) {
-    get(&BoardVertexData<T>::data, m_Board)[vertex];
+  template <typename Iter>
+  Iter selectRandom(Iter start, Iter end) {
+    static std::random_device rd;
+    static std::mt19937 gen(rd());
+    return selectRandom(start, end, gen);
   }
+
+  class Visitor : public boost::default_bfs_visitor {
+   public:
+    Visitor(const Relationships& relationships)
+        : m_Relationships(relationships) {}
+    void discover_vertex(Board::vertex_descriptor currentVertex,
+                         const Board& G) {
+      if (G[currentVertex].potentialStates.size() == 1) {
+        return;
+      }
+
+      auto currentEdges = out_edges(currentVertex, G);
+      std::unordered_set<typename Relationships::vertex_descriptor>
+          compatibleStates;
+      for (auto cEdgeIter = currentEdges.first;
+           cEdgeIter != currentEdges.second; cEdgeIter++) {
+        // Get neighbor along current edge
+        auto neighborVertex = target(*cEdgeIter, G);
+        // Get the returning edge from neighbor so we can get the returning
+        // dimension
+        auto invEdgePair = edge(neighborVertex, currentVertex, G);
+        if (!invEdgePair.second) {
+          throw new std::domain_error("Shit's broke");
+        }
+
+        std::unordered_set<typename Relationships::vertex_descriptor>
+            nCompatibleStates;
+        for (auto state : G[neighborVertex].potentialStates) {
+          // Get all potential states for current vertex based on relationships
+          // and neighbor potential states
+          auto neighborEdges = out_edges(state, m_Relationships);
+          for (auto nEdgeIter = neighborEdges.first;
+               nEdgeIter != neighborEdges.second; nEdgeIter++) {
+            if (m_Relationships[*nEdgeIter].dimension !=
+                G[invEdgePair.first].dimension) {
+              continue;
+            }
+            // Build set of states compatible with neighbor vertex
+            nCompatibleStates.insert(target(*nEdgeIter, m_Relationships));
+          }
+        }
+        // Intersection of compatible states
+        if (compatibleStates.empty()) {
+          compatibleStates = nCompatibleStates;
+        } else {
+          std::erase_if(compatibleStates,
+                        [&nCompatibleStates](
+                            typename Relationships::vertex_descriptor e) {
+                          return !nCompatibleStates.contains(e);
+                        });
+        }
+      }
+
+      // Set current vertex potential states to compatible states
+      G[currentVertex].potentialStates =
+          std::vector(compatibleStates.begin(), compatibleStates.end());
+    }
+
+   private:
+    const Relationships& m_Relationships;
+  };
 };
 
 }  // namespace wfc
--- src/wave_function_collapse.cpp
+++ src/wave_function_collapse.cpp
@@ -1,411 +1,198 @@
-#include <boost/graph/adjacency_list.hpp>
-#include <boost/graph/breadth_first_search.hpp>
-#include <boost/graph/graphviz.hpp>
-#include <boost/graph/named_function_params.hpp>
-#include <boost/graph/properties.hpp>
-#include <boost/property_map/property_map.hpp>
-#include <iostream>
-#include <iterator>
-#include <random>
-#include <ranges>
-#include <string>
-// #include <wave_function_collapse/wave_function_collapse.hpp>
+#include <map>
+#include <memory>
+#include <wave_function_collapse/wave_function_collapse.hpp>
 
 template <typename T>
-struct RelVertexData;
-template <typename T>
-struct BoardVertexData;
-struct EdgeData;
-
-template <typename T>
-using tmpRelationships =
-    boost::adjacency_list<boost::vecS, boost::vecS, boost::bidirectionalS,
-                          RelVertexData<T>, EdgeData, boost::no_property,
-                          boost::listS>;
-
-template <typename T>
-using tmpBoard =
-    boost::adjacency_list<boost::vecS, boost::vecS, boost::bidirectionalS,
-                          BoardVertexData<T>, EdgeData, boost::no_property,
-                          boost::listS>;
-
-template <typename T>
-struct RelVertexData {
-  T data;
-};
-
-template <typename T>
-struct BoardVertexData {
-  std::vector<
-      typename tmpRelationships<T>::vertex_descriptor> mutable potentialStates;
-  unsigned entropy() const { return potentialStates.size(); };
-};
-
-struct EdgeData {
-  unsigned dimension;
-};
-
-template <typename T>
-void createVertices(tmpRelationships<T>& R) {
-  add_vertex({0}, R);
-  add_vertex({1}, R);
-  add_vertex({2}, R);
-  add_vertex({3}, R);
-  add_vertex({4}, R);
-}
-
-template <typename T>
-void createRelationships(tmpRelationships<T>& R) {
+void addRelationships(wfc::WaveFunctionCollapse<T>& w) {
   // Edges for vertex 0
-  add_edge(0, 0, {0}, R).first;
-  add_edge(0, 0, {1}, R);
-  add_edge(0, 0, {2}, R);
-  add_edge(0, 0, {3}, R);
+  w.addRelationship(0, 0, 0);
+  w.addRelationship(0, 0, 1);
+  w.addRelationship(0, 0, 2);
+  w.addRelationship(0, 0, 3);
 
-  add_edge(0, 1, {1}, R);
+  w.addRelationship(0, 1, 1);
 
-  add_edge(0, 2, {2}, R);
+  w.addRelationship(0, 2, 2);
 
-  add_edge(0, 3, {3}, R);
+  w.addRelationship(0, 3, 3);
 
-  add_edge(0, 4, {0}, R);
+  w.addRelationship(0, 4, 0);
 
   // Edges for vertex 1
-  add_edge(1, 0, {3}, R);
+  w.addRelationship(1, 0, 3);
 
-  add_edge(1, 1, {0}, R);
-  add_edge(1, 1, {2}, R);
+  w.addRelationship(1, 1, 0);
+  w.addRelationship(1, 1, 2);
 
-  add_edge(1, 2, {0}, R);
-  add_edge(1, 2, {1}, R);
+  w.addRelationship(1, 2, 0);
+  w.addRelationship(1, 2, 1);
 
-  add_edge(1, 3, {0}, R);
-  add_edge(1, 3, {1}, R);
-  add_edge(1, 3, {2}, R);
-  add_edge(1, 3, {3}, R);
+  w.addRelationship(1, 3, 0);
+  w.addRelationship(1, 3, 1);
+  w.addRelationship(1, 3, 2);
+  w.addRelationship(1, 3, 3);
 
-  add_edge(1, 4, {1}, R);
-  add_edge(1, 4, {2}, R);
+  w.addRelationship(1, 4, 1);
+  w.addRelationship(1, 4, 2);
 
   // Edges for vertex 2
-  add_edge(2, 0, {0}, R);
+  w.addRelationship(2, 0, 0);
 
-  add_edge(2, 1, {2}, R);
-  add_edge(2, 1, {3}, R);
+  w.addRelationship(2, 1, 2);
+  w.addRelationship(2, 1, 3);
 
-  add_edge(2, 2, {1}, R);
-  add_edge(2, 2, {3}, R);
+  w.addRelationship(2, 2, 1);
+  w.addRelationship(2, 2, 3);
 
-  add_edge(2, 3, {1}, R);
-  add_edge(2, 3, {2}, R);
+  w.addRelationship(2, 3, 1);
+  w.addRelationship(2, 3, 2);
 
-  add_edge(2, 4, {0}, R);
-  add_edge(2, 4, {1}, R);
-  add_edge(2, 4, {2}, R);
-  add_edge(2, 4, {3}, R);
+  w.addRelationship(2, 4, 0);
+  w.addRelationship(2, 4, 1);
+  w.addRelationship(2, 4, 2);
+  w.addRelationship(2, 4, 3);
 
   // Edges for vertex 3
-  add_edge(3, 0, {1}, R);
+  w.addRelationship(3, 0, 1);
 
-  add_edge(3, 1, {0}, R);
-  add_edge(3, 1, {1}, R);
-  add_edge(3, 1, {2}, R);
-  add_edge(3, 1, {3}, R);
+  w.addRelationship(3, 1, 0);
+  w.addRelationship(3, 1, 1);
+  w.addRelationship(3, 1, 2);
+  w.addRelationship(3, 1, 3);
 
-  add_edge(3, 2, {0}, R);
-  add_edge(3, 2, {3}, R);
+  w.addRelationship(3, 2, 0);
+  w.addRelationship(3, 2, 3);
 
-  add_edge(3, 3, {0}, R);
-  add_edge(3, 3, {2}, R);
+  w.addRelationship(3, 3, 0);
+  w.addRelationship(3, 3, 2);
 
-  add_edge(3, 4, {2}, R);
-  add_edge(3, 4, {3}, R);
+  w.addRelationship(3, 4, 2);
+  w.addRelationship(3, 4, 3);
 
   // Edges for vertex 4
-  add_edge(4, 0, {2}, R);
+  w.addRelationship(4, 0, 2);
 
-  add_edge(4, 1, {0}, R);
-  add_edge(4, 1, {3}, R);
+  w.addRelationship(4, 1, 0);
+  w.addRelationship(4, 1, 3);
 
-  add_edge(4, 2, {0}, R);
-  add_edge(4, 2, {1}, R);
-  add_edge(4, 2, {2}, R);
-  add_edge(4, 2, {3}, R);
+  w.addRelationship(4, 2, 0);
+  w.addRelationship(4, 2, 1);
+  w.addRelationship(4, 2, 2);
+  w.addRelationship(4, 2, 3);
 
-  add_edge(4, 3, {0}, R);
-  add_edge(4, 3, {1}, R);
+  w.addRelationship(4, 3, 0);
+  w.addRelationship(4, 3, 1);
 
-  add_edge(4, 4, {1}, R);
-  add_edge(4, 4, {3}, R);
+  w.addRelationship(4, 4, 1);
+  w.addRelationship(4, 4, 3);
 }
 
 template <typename T>
-void createBoard(tmpBoard<T>& B) {
+void addNeighbors(wfc::WaveFunctionCollapse<T>& w) {
   // Row 1
-  add_edge(0, 1, {2}, B);
-  add_edge(0, 4, {3}, B);
+  w.addNeighbor(0, 1, 2);
+  w.addNeighbor(0, 4, 3);
 
-  add_edge(1, 0, {0}, B);
-  add_edge(1, 2, {2}, B);
-  add_edge(1, 5, {3}, B);
+  w.addNeighbor(1, 0, 0);
+  w.addNeighbor(1, 2, 2);
+  w.addNeighbor(1, 5, 3);
 
-  add_edge(2, 1, {0}, B);
-  add_edge(2, 3, {2}, B);
-  add_edge(2, 6, {3}, B);
+  w.addNeighbor(2, 1, 0);
+  w.addNeighbor(2, 3, 2);
+  w.addNeighbor(2, 6, 3);
 
-  add_edge(3, 2, {0}, B);
-  add_edge(3, 7, {3}, B);
+  w.addNeighbor(3, 2, 0);
+  w.addNeighbor(3, 7, 3);
 
   // Row 2
-  add_edge(4, 0, {1}, B);
-  add_edge(4, 5, {2}, B);
-  add_edge(4, 8, {3}, B);
+  w.addNeighbor(4, 0, 1);
+  w.addNeighbor(4, 5, 2);
+  w.addNeighbor(4, 8, 3);
 
-  add_edge(5, 1, {1}, B);
-  add_edge(5, 4, {0}, B);
-  add_edge(5, 6, {2}, B);
-  add_edge(5, 9, {3}, B);
+  w.addNeighbor(5, 1, 1);
+  w.addNeighbor(5, 4, 0);
+  w.addNeighbor(5, 6, 2);
+  w.addNeighbor(5, 9, 3);
 
-  add_edge(6, 2, {1}, B);
-  add_edge(6, 5, {0}, B);
-  add_edge(6, 7, {2}, B);
-  add_edge(6, 10, {3}, B);
+  w.addNeighbor(6, 2, 1);
+  w.addNeighbor(6, 5, 0);
+  w.addNeighbor(6, 7, 2);
+  w.addNeighbor(6, 10, 3);
 
-  add_edge(7, 3, {1}, B);
-  add_edge(7, 6, {0}, B);
-  add_edge(7, 11, {3}, B);
+  w.addNeighbor(7, 3, 1);
+  w.addNeighbor(7, 6, 0);
+  w.addNeighbor(7, 11, 3);
 
   // Row 3
-  add_edge(8, 4, {1}, B);
-  add_edge(8, 9, {2}, B);
-  add_edge(8, 12, {3}, B);
+  w.addNeighbor(8, 4, 1);
+  w.addNeighbor(8, 9, 2);
+  w.addNeighbor(8, 12, 3);
 
-  add_edge(9, 5, {1}, B);
-  add_edge(9, 8, {0}, B);
-  add_edge(9, 10, {2}, B);
-  add_edge(9, 13, {3}, B);
+  w.addNeighbor(9, 5, 1);
+  w.addNeighbor(9, 8, 0);
+  w.addNeighbor(9, 10, 2);
+  w.addNeighbor(9, 13, 3);
 
-  add_edge(10, 6, {1}, B);
-  add_edge(10, 9, {0}, B);
-  add_edge(10, 11, {2}, B);
-  add_edge(10, 14, {3}, B);
+  w.addNeighbor(10, 6, 1);
+  w.addNeighbor(10, 9, 0);
+  w.addNeighbor(10, 11, 2);
+  w.addNeighbor(10, 14, 3);
 
-  add_edge(11, 7, {1}, B);
-  add_edge(11, 10, {0}, B);
-  add_edge(11, 15, {3}, B);
+  w.addNeighbor(11, 7, 1);
+  w.addNeighbor(11, 10, 0);
+  w.addNeighbor(11, 15, 3);
 
   // Row 4
-  add_edge(12, 8, {1}, B);
-  add_edge(12, 13, {2}, B);
+  w.addNeighbor(12, 8, 1);
+  w.addNeighbor(12, 13, 2);
 
-  add_edge(13, 9, {1}, B);
-  add_edge(13, 12, {0}, B);
-  add_edge(13, 14, {2}, B);
+  w.addNeighbor(13, 9, 1);
+  w.addNeighbor(13, 12, 0);
+  w.addNeighbor(13, 14, 2);
 
-  add_edge(14, 10, {1}, B);
-  add_edge(14, 13, {0}, B);
-  add_edge(14, 15, {2}, B);
+  w.addNeighbor(14, 10, 1);
+  w.addNeighbor(14, 13, 0);
+  w.addNeighbor(14, 15, 2);
 
-  add_edge(15, 11, {1}, B);
-  add_edge(15, 14, {0}, B);
-}
-
-template <typename T>
-void printRelationships(tmpRelationships<T>& R) {
-  std::cout << "Relationships:" << std::endl;
-  auto vpair = vertices(R);
-  for (auto iter = vpair.first; iter != vpair.second; iter++) {
-    std::cout << "vertex: " << *iter
-              << " data: " << get(&RelVertexData<int>::data, R)[*iter]
-              << std::endl;
-  }
-
-  auto epair = edges(R);
-  for (auto iter = epair.first; iter != epair.second; iter++) {
-    std::cout << "edge " << source(*iter, R) << " - " << target(*iter, R)
-              << " has dimension " << get(&EdgeData::dimension, R)[*iter]
-              << std::endl;
-  }
-}
-
-template <typename T>
-void printBoard(tmpBoard<T>& B) {
-  std::cout << "\nBoard:" << std::endl;
-  auto vpair = vertices(B);
-  for (auto iter = vpair.first; iter != vpair.second; iter++) {
-    std::cout << "vertex: " << *iter;
-    if (B[*iter].potentialStates.size() == 1) {
-      std::cout << " data: " << B[*iter].potentialStates[0];
-    }
-    std::cout << " entropy: " << B[*iter].entropy() << std::endl;
-  }
-
-  // auto epair = edges(B);
-  // for (auto iter = epair.first; iter != epair.second; iter++) {
-  //   std::cout << "edge " << source(*iter, B) << " - " << target(*iter, B)
-  //             << " has dimension " << get(&EdgeData::dimension, B)[*iter]
-  //             << std::endl;
-  // }
-}
-
-template <typename T>
-void printAll(tmpBoard<T>& B, tmpRelationships<T>& R) {
-  printRelationships<T>(R);
-  printBoard<T>(B);
-}
-
-template <typename T>
-class BFSWFCVisitor : public boost::default_bfs_visitor {
- public:
-  BFSWFCVisitor(const tmpRelationships<T>& R) : m_relationships(R) {}
-  template <typename Vertex, typename Graph>
-  void discover_vertex(Vertex currentVertex, Graph& G) {
-    if (G[currentVertex].potentialStates.size() == 1) {
-      // std::cout << "Vertex " << currentVertex << " already collapsed.
-      // Skipping."
-      //           << std::endl;
-      return;
-    }
-    // std::cout << "Visited vertex " << currentVertex << std::endl;
-
-    auto currentEdges = out_edges(currentVertex, G);
-    std::unordered_set<typename tmpRelationships<T>::vertex_descriptor>
-        compatibleStates;
-    for (auto cEdgeIter = currentEdges.first; cEdgeIter != currentEdges.second;
-         cEdgeIter++) {
-      // std::cout << "Checking edge with dimension " << G[*cEdgeIter].dimension
-      //           << std::endl;
-      // Get neighbor along current edge
-      auto neighborVertex = target(*cEdgeIter, G);
-      // Get the returning edge from neighbor so we can get the returning
-      // dimension
-      auto invEdgePair = edge(neighborVertex, currentVertex, G);
-      if (!invEdgePair.second) {
-        throw new std::domain_error("Shit's broke");
-      }
-
-      std::unordered_set<typename tmpRelationships<T>::vertex_descriptor>
-          nCompatibleStates;
-      for (auto state : G[neighborVertex].potentialStates) {
-        // Get all potential states for current vertex based on relationships
-        // and neighbor potential states
-        auto neighborEdges = out_edges(state, m_relationships);
-        for (auto nEdgeIter = neighborEdges.first;
-             nEdgeIter != neighborEdges.second; nEdgeIter++) {
-          if (m_relationships[*nEdgeIter].dimension !=
-              G[invEdgePair.first].dimension) {
-            continue;
-          }
-          // Build set of states compatible with neighbor vertex
-          nCompatibleStates.insert(target(*nEdgeIter, m_relationships));
-        }
-      }
-      // Intersection of compatible states
-      if (compatibleStates.empty()) {
-        compatibleStates = nCompatibleStates;
-      } else {
-        std::erase_if(compatibleStates,
-                      [&nCompatibleStates](
-                          typename tmpRelationships<T>::vertex_descriptor e) {
-                        return !nCompatibleStates.contains(e);
-                      });
-      }
-    }
-
-    // Set current vertex potential states to compatible states
-    G[currentVertex].potentialStates =
-        std::vector(compatibleStates.begin(), compatibleStates.end());
-  }
-
- private:
-  const tmpRelationships<T>& m_relationships;
-};
-
-// template <typename T>
-// void updateNeighbors(tmpBoard<T>& B, const tmpRelationships<T>& R,
-//                      const typename tmpBoard<T>::vertex_descriptor bV) {
-//   // auto epair = out_edges(bV, B);
-//   // for (auto iter = epair.first; iter != epair.second; iter++) {
-//   //   std::cout << "Out edge from " << source(*iter, B) << " - "
-//   //             << target(*iter, B) << " has dimension "
-//   //             << get(&EdgeData::dimension, B)[*iter] << std::endl;
-//   // }
-
-//   boost::breadth_first_search(B, bV, boost::visitor(BFSWFCVisitor<T>(R)));
-// }
-
-template <typename T>
-void collapse(tmpBoard<T>& B, const tmpRelationships<T>& R,
-              const typename tmpBoard<T>::vertex_descriptor bV,
-              const typename tmpRelationships<T>::vertex_descriptor rV) {
-  B[bV].potentialStates = {rV};
-
-  // updateNeighbors<T>(B, R, bV);
-  boost::breadth_first_search(B, bV, boost::visitor(BFSWFCVisitor<T>(R)));
-}
-
-template <typename Iter, typename RandomGenerator>
-Iter select_randomly(Iter start, Iter end, RandomGenerator& g) {
-  std::uniform_int_distribution<> dis(0, std::distance(start, end) - 1);
-  std::advance(start, dis(g));
-  return start;
-}
-
-template <typename Iter>
-Iter select_randomly(Iter start, Iter end) {
-  static std::random_device rd;
-  static std::mt19937 gen(rd());
-  return select_randomly(start, end, gen);
+  w.addNeighbor(15, 11, 1);
+  w.addNeighbor(15, 14, 0);
 }
 
 int main(int argc, char const* argv[]) {
-  tmpRelationships<int> R;
-  createVertices<int>(R);
-  createRelationships<int>(R);
+  std::map<unsigned, std::shared_ptr<int>> resources;
 
-  tmpBoard<int> B(16);
+  wfc::WaveFunctionCollapse<int> w(16);
 
-  createBoard<int>(B);
+  {
+    auto tmpData = std::make_shared<int>(0);
+    auto tmpVertex = w.addResource(tmpData);
+    resources[tmpVertex] = tmpData;
 
-  auto bvpair = vertices(B);
-  auto rvpair = vertices(R);
-  for (auto iter = bvpair.first; iter != bvpair.second; iter++) {
-    B[*iter].potentialStates = std::vector(rvpair.first, rvpair.second);
+    tmpData = std::make_shared<int>(1);
+    tmpVertex = w.addResource(tmpData);
+    resources[tmpVertex] = tmpData;
+
+    tmpData = std::make_shared<int>(2);
+    tmpVertex = w.addResource(tmpData);
+    resources[tmpVertex] = tmpData;
+
+    tmpData = std::make_shared<int>(3);
+    tmpVertex = w.addResource(tmpData);
+    resources[tmpVertex] = tmpData;
+
+    tmpData = std::make_shared<int>(4);
+    tmpVertex = w.addResource(tmpData);
+    resources[tmpVertex] = tmpData;
   }
 
-  // printAll<int>(B, R);
+  addRelationships(w);
+  addNeighbors(w);
 
-  collapse(B, R, 5, 1);
-
-  for (unsigned i = 0; i < num_vertices(B); i++) {
-    typename tmpBoard<int>::vertex_descriptor min = 0;
-    for (auto iter = bvpair.first; iter != bvpair.second; iter++) {
-      if ((B[*iter].entropy() != 1 && min == 0) ||
-          (B[*iter].entropy() != 1 && B[*iter].entropy() < B[min].entropy())) {
-        min = *iter;
-      }
-    }
-    // vec | std::ranges::views::filter([](int a) { return a % 2 == 0; });
-    // auto minEntropyIter =
-    //     std::min_element(bvpair.first, bvpair.second,
-    //                      [&B](typename tmpBoard<int>::vertex_descriptor v1,
-    //                           typename tmpBoard<int>::vertex_descriptor v2) {
-    //                        if (B[v1].entropy() == 1) {
-    //                          return false;
-    //                        }
-    //                        return B[v1].entropy() <= B[v2].entropy();
-    //                      });
-    // std::cout << "Next vertex descriptor: " << min << std::endl;
-    // if (B[*minEntropyIter].entropy() == 1) {
-    //   break;
-    // }
-
-    collapse(B, R, min,
-             *select_randomly(B[min].potentialStates.begin(),
-                              B[min].potentialStates.end()));
-  }
-
-  printBoard<int>(B);
+  w.init();
+  // w.collapse(5, 4); // This is ok by itself
+  // w.collapse(9, 0); // This throws if both lines uncommented
+  w.run();
+  w.printBoard();
 
   return 0;
 }
