6f8d75745fca43ff29e1c08fa108785ead28532b
Logan Thomas <ltogan.2014@gmail.com>
2025-06-08 09:00:13 -0400

    Switch to boost graph lib

--- include/wave_function_collapse/wave_function_collapse.hpp
+++ include/wave_function_collapse/wave_function_collapse.hpp
@@ -1,3 +1,57 @@
 #pragma once
 
-#include <iostream>
+#include <boost/graph/adjacency_list.hpp>
+#include <boost/graph/adjacency_matrix.hpp>
+#include <boost/graph/graphviz.hpp>
+#include <boost/graph/named_function_params.hpp>
+#include <boost/graph/properties.hpp>
+#include <boost/property_map/property_map.hpp>
+
+namespace wfc {
+
+template <typename T>
+struct RelVertexData {
+  T data;
+};
+
+template <typename T>
+struct BoardVertexData : public RelVertexData<T> {
+  unsigned entropy;
+};
+
+struct EdgeData {
+  unsigned dimension;
+};
+
+template <typename T>
+class wfc {
+ private:
+  using Relationships =
+      boost::adjacency_list<boost::vecS, boost::vecS, boost::directedS,
+                            RelVertexData<T>, EdgeData, boost::no_property,
+                            boost::listS>;
+  using Board =
+      boost::adjacency_matrix<boost::undirectedS, BoardVertexData<T>, EdgeData,
+                              boost::no_property, std::allocator<bool>>;
+
+  Relationships m_Relationships;
+  Board m_Board;
+
+ public:
+  bool addRelationship(unsigned v1, unsigned v2, unsigned dimension) {
+    return false;
+  }
+
+  bool addNeighbor(unsigned v1, unsigned v2) { return false; }
+
+ private:
+  inline T& getRelationshipData(unsigned vertex) {
+    get(&RelVertexData<T>::data, m_Relationships)[vertex];
+  }
+
+  inline T& getBoardData(unsigned vertex) {
+    get(&BoardVertexData<T>::data, m_Board)[vertex];
+  }
+};
+
+}  // namespace wfc
--- src/wave_function_collapse.cpp
+++ src/wave_function_collapse.cpp
@@ -1,45 +1,187 @@
+#include <boost/graph/adjacency_list.hpp>
+#include <boost/graph/graphviz.hpp>
+#include <boost/graph/named_function_params.hpp>
+#include <boost/graph/properties.hpp>
+#include <boost/property_map/property_map.hpp>
 #include <iostream>
 #include <string>
+#include <wave_function_collapse/wave_function_collapse.hpp>
 
-#include <wave_function_collapse/graph/graph.hpp>
+template <typename T>
+using tmpRelationships =
+    boost::adjacency_list<boost::vecS, boost::vecS, boost::directedS,
+                          wfc::RelVertexData<T>, wfc::EdgeData,
+                          boost::no_property, boost::listS>;
 
-class MyClass
-{
-public:
-  MyClass(const std::string& name): name_(name) {}
+template <typename T>
+void createRelationships(tmpRelationships<T>& R) {
+  auto v0 = add_vertex(R);
+  R[v0].data = 0;
+  auto v1 = add_vertex(R);
+  R[v1].data = 1;
+  auto v2 = add_vertex(R);
+  R[v2].data = 2;
+  auto v3 = add_vertex(R);
+  R[v3].data = 3;
+  auto v4 = add_vertex(R);
+  R[v4].data = 4;
 
-private:
-  std::string name_;
-};
+  // Edges for vertex 0
+  auto e1 = add_edge(v0, v0, R).first;
+  R[e1].dimension = 0;
+  e1 = add_edge(v0, v0, R).first;
+  R[e1].dimension = 1;
+  e1 = add_edge(v0, v0, R).first;
+  R[e1].dimension = 2;
+  e1 = add_edge(v0, v0, R).first;
+  R[e1].dimension = 3;
 
-namespace wfc = wave_function_collapse;
-int main(int argc, char const *argv[])
-{
-  wfc::Graph<int> graph1;
-  graph1.addVertex(1);
-  graph1.addVertex(2);
+  e1 = add_edge(v0, v1, R).first;
+  R[e1].dimension = 1;
 
-  wfc::Graph<int> graph2;
-  graph2.addVertex(3);
-  graph2.addVertex(4);
+  e1 = add_edge(v0, v2, R).first;
+  R[e1].dimension = 2;
 
-  // wfc::Graph<wfc::Graph<int>> graph3;
-  // graph3.addVertex(graph1);
-  // graph3.addVertex(graph2);
+  e1 = add_edge(v0, v3, R).first;
+  R[e1].dimension = 3;
 
-  // std::cout << "Graph 1: [ ";
-  // for (auto vertex : graph1)
-  // {
-  //   std::cout << vertex << " ";
-  // }
-  // std::cout << "]" << std::endl;
+  e1 = add_edge(v0, v4, R).first;
+  R[e1].dimension = 0;
 
-  // std::cout << "Graph 2: [ ";
-  // for (auto vertex : graph2)
-  // {
-  //   std::cout << vertex << " ";
-  // }
-  // std::cout << "]" << std::endl;
+  // Edges for vertex 1
+  e1 = add_edge(v1, v0, R).first;
+  R[e1].dimension = 3;
+
+  e1 = add_edge(v1, v1, R).first;
+  R[e1].dimension = 0;
+  e1 = add_edge(v1, v1, R).first;
+  R[e1].dimension = 2;
+
+  e1 = add_edge(v1, v2, R).first;
+  R[e1].dimension = 0;
+  e1 = add_edge(v1, v2, R).first;
+  R[e1].dimension = 1;
+
+  e1 = add_edge(v1, v3, R).first;
+  R[e1].dimension = 0;
+  e1 = add_edge(v1, v3, R).first;
+  R[e1].dimension = 1;
+  e1 = add_edge(v1, v3, R).first;
+  R[e1].dimension = 2;
+  e1 = add_edge(v1, v3, R).first;
+  R[e1].dimension = 3;
+
+  e1 = add_edge(v1, v4, R).first;
+  R[e1].dimension = 1;
+  e1 = add_edge(v1, v4, R).first;
+  R[e1].dimension = 2;
+
+  // Edges for vertex 2
+  e1 = add_edge(v2, v0, R).first;
+  R[e1].dimension = 0;
+
+  e1 = add_edge(v2, v1, R).first;
+  R[e1].dimension = 2;
+  e1 = add_edge(v2, v1, R).first;
+  R[e1].dimension = 3;
+
+  e1 = add_edge(v2, v2, R).first;
+  R[e1].dimension = 1;
+  e1 = add_edge(v2, v2, R).first;
+  R[e1].dimension = 3;
+
+  e1 = add_edge(v2, v3, R).first;
+  R[e1].dimension = 1;
+  e1 = add_edge(v2, v3, R).first;
+  R[e1].dimension = 2;
+
+  e1 = add_edge(v2, v4, R).first;
+  R[e1].dimension = 0;
+  e1 = add_edge(v2, v4, R).first;
+  R[e1].dimension = 1;
+  e1 = add_edge(v2, v4, R).first;
+  R[e1].dimension = 2;
+  e1 = add_edge(v2, v4, R).first;
+  R[e1].dimension = 3;
+
+  // Edges for vertex 3
+  e1 = add_edge(v3, v0, R).first;
+  R[e1].dimension = 1;
+
+  e1 = add_edge(v3, v1, R).first;
+  R[e1].dimension = 0;
+  e1 = add_edge(v3, v1, R).first;
+  R[e1].dimension = 1;
+  e1 = add_edge(v3, v1, R).first;
+  R[e1].dimension = 2;
+  e1 = add_edge(v3, v1, R).first;
+  R[e1].dimension = 3;
+
+  e1 = add_edge(v3, v2, R).first;
+  R[e1].dimension = 0;
+  e1 = add_edge(v3, v2, R).first;
+  R[e1].dimension = 3;
+
+  e1 = add_edge(v3, v3, R).first;
+  R[e1].dimension = 0;
+  e1 = add_edge(v3, v3, R).first;
+  R[e1].dimension = 2;
+
+  e1 = add_edge(v3, v4, R).first;
+  R[e1].dimension = 2;
+  e1 = add_edge(v3, v4, R).first;
+  R[e1].dimension = 3;
+
+  // Edges for vertex 4
+  e1 = add_edge(v4, v0, R).first;
+  R[e1].dimension = 2;
+
+  e1 = add_edge(v4, v1, R).first;
+  R[e1].dimension = 0;
+  e1 = add_edge(v4, v1, R).first;
+  R[e1].dimension = 3;
+
+  e1 = add_edge(v4, v2, R).first;
+  R[e1].dimension = 0;
+  e1 = add_edge(v4, v2, R).first;
+  R[e1].dimension = 1;
+  e1 = add_edge(v4, v2, R).first;
+  R[e1].dimension = 2;
+  e1 = add_edge(v4, v2, R).first;
+  R[e1].dimension = 3;
+  e1 = add_edge(v4, v2, R).first;
+  R[e1].dimension = 2;
+  e1 = add_edge(v4, v2, R).first;
+  R[e1].dimension = 3;
+
+  e1 = add_edge(v4, v3, R).first;
+  R[e1].dimension = 0;
+  e1 = add_edge(v4, v3, R).first;
+  R[e1].dimension = 1;
+
+  e1 = add_edge(v4, v4, R).first;
+  R[e1].dimension = 1;
+  e1 = add_edge(v4, v4, R).first;
+  R[e1].dimension = 3;
+}
+
+int main(int argc, char const* argv[]) {
+  tmpRelationships<int> R;
+  createRelationships<int>(R);
+
+  auto vpair = vertices(R);
+  for (auto iter = vpair.first; iter != vpair.second; iter++) {
+    std::cout << "vertex: " << *iter
+              << " data: " << get(&wfc::RelVertexData<int>::data, R)[*iter]
+              << std::endl;
+  }
+
+  auto epair = edges(R);
+  for (auto iter = epair.first; iter != epair.second; iter++) {
+    std::cout << "edge " << source(*iter, R) << " - " << target(*iter, R)
+              << " has dimension " << get(&wfc::EdgeData::dimension, R)[*iter]
+              << std::endl;
+  }
 
   return 0;
 }
